981d1868422291f12f10403e7747dedf85bf6960
Shubham Barudwale <barudwaleshubham.dinesh2015@vit.ac.in>
2018-05-25 02:17:23 +0530

    Secured Changes in DB upload

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -27,7 +27,7 @@ module Api
           backdir = Dir.getwd;
           Dir.chdir('public/AdvocateProfilesText')
 
-          command = 'curl -X POST --user 1160d099-802a-410f-b582-b9b5678d5c80:EJ2ARC2i4Twf --header "Content-Type: text/plain;charset=utf-8" --header "Accept: application/json"' + ' ' + "--data-binary " + '"' + "@#{datafile}" + '"' + " --output profileout.json " + '"' + "https://gateway.watsonplatform.net/personality-insights/api/v2/profile?version=2017-10-13" + '"';
+          command = 'curl -X POST --user ********-****-****-****-********:******** --header "Content-Type: text/plain;charset=utf-8" --header "Accept: application/json"' + ' ' + "--data-binary " + '"' + "@#{datafile}" + '"' + " --output profileout.json " + '"' + "https://gateway.watsonplatform.net/personality-insights/api/v2/profile?version=2017-10-13" + '"';
           system(command)
 
           FileUtils.mv 'profileout.json', backdir, :force => true
